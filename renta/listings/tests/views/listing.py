5a3d8cc183c0ef38642365cccd76c15c6390d3b0
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-07-31 08:35:12 +0800

    Add initial create listing tests

--- renta/listings/tests/views/listing.py
+++ renta/listings/tests/views/listing.py
@@ -8,6 +8,8 @@ from accounts.tests.factories import UserFactory
 
 from faker import Faker
 
+from io import BytesIO
+from PIL import Image
 
 fake = Faker()
 
@@ -26,8 +28,24 @@ class ListingViewTestCase(BaseRentaTestCase):
         self.listing_one = ListingFactory()
 
         self.listing_update_data = { 'title': fake.color_name() }
+        self.listing_valid_create_data = {
+            'title': fake.color_name(),
+            'description': fake.sentence(nb_words=10),
+            'price': 555,
+            'location': fake.address(),
+            'location_coordinates': f'{fake.latitude()},{fake.longitude()}',
+            'images': [self.create_test_image(), self.create_test_image(), ]
+        }
 
         self.sign_in_as_user(self.user)
+
+    def create_test_image(self):
+        file = BytesIO()
+        image = Image.new('RGBA', size=(50, 50), color=(155, 0, 0))
+        image.save(file, 'png')
+        file.name = 'test.png'
+        file.seek(0)
+        return file
 
     def test_list_listings_by_anon_succeeds(self):
         self.client.credentials()
@@ -133,3 +151,49 @@ class ListingViewTestCase(BaseRentaTestCase):
         response = self.client.delete(self.get_detail_url(self.listing_one.id))
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_create_listing_by_anon_with_valid_data_fails(self):
+        self.client.credentials()
+
+        response = self.client.post(self.get_list_url(), self.listing_valid_create_data)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_listing_by_anon_with_invalid_data_fails(self):
+        self.client.credentials()
+
+        response = self.client.post(self.get_list_url(), {'title': 'Hello world.'})
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_listing_by_auth_user_with_valid_data_succeeds(self):
+        user = UserFactory()
+
+        self.sign_in_as_user(user)
+
+        response = self.client.post(self.get_list_url(), self.listing_valid_create_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_listing_by_auth_user_with_invalid_data_fails(self):
+        user = UserFactory()
+
+        self.sign_in_as_user(user)
+
+        response = self.client.post(self.get_list_url(), {'title': 'Hello?'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_listing_by_admin_valid_data_succeeds(self):
+        self.sign_in_as_user(self.admin)
+
+        response = self.client.post(self.get_list_url(), self.listing_valid_create_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_listing_by_admin_valid_data_fails(self):
+        self.sign_in_as_user(self.admin)
+
+        response = self.client.post(self.get_list_url(), {'title': 'Hello?'})
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
