e6983fef52f88b4b1d5d6bf16e4b3e880f54710e
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-08-02 07:53:30 +0800

    Add initial form validation to create listing form

--- assets/pages/listings/create.js
+++ assets/pages/listings/create.js
@@ -1,6 +1,10 @@
 import {Component} from 'react';
 import nextCookie from 'next-cookies';
+import {mapValues, get} from 'lodash';
+import fetch from 'isomorphic-unfetch';
+
 import DefaultLayout from '../../components/layouts/default';
+import { LISTINGS_API_URL } from '../../common/constants/api';
 
 
 class Create extends Component {
@@ -15,9 +19,12 @@ class Create extends Component {
         billing_frequency: { value: '' },
         location: { value: '' },
         location_coordinates: { value: '' }
-      }
+      },
+      error: {},
+      success: false,
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
@@ -35,69 +42,121 @@ class Create extends Component {
     });
   }
 
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    const {token} = nextCookie(this);
+    const url = LISTINGS_API_URL();
+    const data = mapValues({...this.state.form}, i => get(i, 'value'));
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        },
+        body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+        // Clear form
+        // Show success message
+        this.setState({success: true});
+      } else {
+        const errorJson = await response.json();
+
+        let error = new Error(response.statusText);
+        error.response = response;
+        error.json = errorJson;
+        throw error;
+      }
+    } catch (error) {
+      this.setState({error: error.json});
+    }
+  }
+
   render() {
     return (
       <DefaultLayout>
-        <h1>Create</h1>
-        <form>
+        <h1>Create Listing</h1>
+        <form onSubmit={this.handleSubmit}>
           <fieldset>
             <div className="row">
-              <label htmlFor="title">Title</label>
-              <input
-                type="text"
-                name="title"
-                id="title"
-                value={this.state.form.title.value}
-                onChange={this.handleChange}
-              />
+              <div className={"form-control " + (this.state.error.title ? 'invalid' : '')}>
+                <label htmlFor="title">Title</label>
+                <span className="error">{this.state.error.title}</span>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={this.state.form.title.value}
+                  onChange={this.handleChange}
+                />
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="description">Description</label>
-              <textarea
-                name="description"
-                id="description"
-                rows="5"
-                value={this.state.form.description.value}
-                onChange={this.handleChange}></textarea>
+              <div className={"form-control " + (this.state.error.description ? 'invalid' : '')}>
+                <label htmlFor="description">Description</label>
+                <span className="error">{this.state.error.description}</span>
+                <textarea
+                  name="description"
+                  id="description"
+                  rows="5"
+                  value={this.state.form.description.value}
+                  onChange={this.handleChange}></textarea>
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="price">Price</label>
-              <input
-                type="number"
-                name="price"
-                id="price"
-                value={this.state.form.price.value}
-                onChange={this.handleChange}/>
+              <div className={"form-control " + (this.state.error.price ? 'invalid' : '')}>
+                <label htmlFor="price">Price</label>
+                <span className="error">{this.state.error.price}</span>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  value={this.state.form.price.value}
+                  onChange={this.handleChange}/>
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="billing_frequency">Billing Frequency</label>
-              <select
-                name="billing_frequency"
-                id="billing_frequency"
-                value={this.state.form.billing_frequency.value}
-                onChange={this.handleChange}>
-                <option value="daily">Daily</option>
-                <option value="monthly">Monthly</option>
-                <option value="yearly">Yearly</option>
-              </select>
+              <div className={"form-control " + (this.state.error.billing_frequency ? 'invalid' : '')}>
+                <label htmlFor="billing_frequency">Billing Frequency</label>
+                <span className="error">{this.state.error.billing_frequency}</span>
+                <select
+                  name="billing_frequency"
+                  id="billing_frequency"
+                  value={this.state.form.billing_frequency.value}
+                  onChange={this.handleChange}>
+                  <option value="daily">Daily</option>
+                  <option value="monthly">Monthly</option>
+                  <option value="yearly">Yearly</option>
+                </select>
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="location">Location</label>
-              <input
-                type="text"
-                name="location"
-                id="location"
-                value={this.state.form.location.value}
-                onChange={this.handleChange}/>
+              <div className={"form-control " + (this.state.error.location ? 'invalid' : '')}>
+                <label htmlFor="location">Location</label>
+                <span className="error">{this.state.error.location}</span>
+                <input
+                  type="text"
+                  name="location"
+                  id="location"
+                  value={this.state.form.location.value}
+                  onChange={this.handleChange}/>
+              </div>
             </div>
             <div className="row">
-              <label htmlFor="location_coordiantes">Coordinates</label>
-              <input
-                type="text"
-                name="location_coordinates"
-                id="location_coordinates"
-                value={this.state.form.location_coordinates.value}
-                onChange={this.handleChange}/>
+              <div className={"form-control " + (this.state.error.location_coordinates ? 'invalid' : '')}>
+                <label htmlFor="location_coordiantes">Coordinates</label>
+                <span className="error">{this.state.error.location_coordiantes}</span>
+                <input
+                  type="text"
+                  name="location_coordinates"
+                  id="location_coordinates"
+                  value={this.state.form.location_coordinates.value}
+                  onChange={this.handleChange}/>
+              </div>
             </div>
           </fieldset>
           <fieldset>
@@ -106,20 +165,26 @@ class Create extends Component {
             </div>
           </fieldset>
         </form>
+
+        <style jsx>{`
+            form .form-control.invalid input,
+            form .form-control.invalid textarea,
+            form .form-control.invalid select {
+              background-color: #fce4e4;
+              border: 1px solid #e55039;
+              outline: none;
+            }
+            form .form-control.invalid label {
+              margin-bottom: 0;
+            }
+            form .form-control span.error {
+              color: #e55039;
+              font-size: .7em;
+            }
+          `}</style>
       </DefaultLayout>
     )
   }
 }
-
-// Create.getInitialProps = async function(context) {
-//   const { token } = nextCookie(context);
-
-//   const redirectOnError = () =>
-//     typeof window !== 'undefined'
-//       ? Router.push('/auth/signin')
-//       : context.res.writeHead(302, { Location: '/auth/signin' }).end();
-
-//   return {};
-// }
 
 export default Create;
