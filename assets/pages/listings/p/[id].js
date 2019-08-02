a476c2105b78b91bf1c67e0382c112678c1bd95c
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-08-03 06:13:09 +0800

    Add empty image state

--- assets/pages/listings/p/[id].js
+++ assets/pages/listings/p/[id].js
@@ -12,10 +12,35 @@ export const Listing = (props) => {
   return (
     <DefaultLayout>
       <h1>Listing</h1>
-      <h2>{listing.title}</h2>
-      <h4>{listing.price}</h4>
-      <small>{listing.location}</small>
-      <p>{listing.description}</p>
+      <div className="row">
+        <div className="images">
+          <div className="image upload">
+            <p>Upload a new Photo</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <h2>{listing.title}</h2>
+        <h4>{listing.price}</h4>
+        <small>{listing.location}</small>
+        <p>{listing.description}</p>
+      </div>
+      <style jsx>{`
+          .images .image {
+            width: 260px;
+            height: 260px;
+          }
+          .images .image.upload {
+            border: 3px dotted #333;
+            background-color: #ccc;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+            text-align: center;
+            align-items: center;
+          }
+        `}</style>
     </DefaultLayout>
   )
 }
