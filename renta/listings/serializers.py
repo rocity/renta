505ad203c8d9356109d2c393231ea222be81939e
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-08-07 11:39:32 +0800

    Add is_user prop to listing serializer

--- renta/listings/serializers.py
+++ renta/listings/serializers.py
@@ -23,14 +23,21 @@ class ListingSerializer(serializers.ModelSerializer):
                                               queryset=get_user_model().objects.all())
     image_urls = serializers.SerializerMethodField()
     owner = serializers.SerializerMethodField()
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Listing
         fields = ('id', 'title', 'description', 'price', 'billing_frequency', 'location',
-                  'location_coordinates', 'user', 'image_urls', 'owner', )
+                  'location_coordinates', 'user', 'image_urls', 'owner', 'is_owner', )
 
     def get_image_urls(self, obj):
         return ListingImageSerializer(instance=obj.listingimage_set.all(), many=True).data
 
     def get_owner(self, obj):
         return obj.user.get_full_name()
+
+    def get_is_owner(self, obj):
+        try:
+            return self.context.get('request').user == obj.user
+        except AttributeError:
+            return False
