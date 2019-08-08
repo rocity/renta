7bf3ac339df1a6698f9c54586ac6336e8c1befad
Kevin Kalis <kevinpaulkalis@gmail.com>
2019-08-08 19:11:49 +0800

    Add listingimage factory

--- renta/listings/tests/factories.py
+++ renta/listings/tests/factories.py
@@ -1,6 +1,8 @@
-from factory import DjangoModelFactory, SubFactory
+from django.core.files.base import ContentFile
+from factory import DjangoModelFactory, SubFactory, LazyAttribute
+from factory.django import ImageField
 
-from listings.models import Listing
+from listings.models import Listing, ListingImage
 from random import randint
 from accounts.tests.factories import UserFactory
 
@@ -29,3 +31,24 @@ class ListingFactory(DjangoModelFactory):
     description = fake.sentence(nb_words=10)
     price = randint(1, 200)
     location = fake.address()
+
+
+class ListingImageFactory(DjangoModelFactory):
+    """
+    Generates a ListingImage Instance
+
+    Default Fields:
+    - listing
+    - image
+    """
+    class Meta:
+        model = ListingImage
+
+    listing = SubFactory(ListingFactory)
+    image = LazyAttribute(
+        lambda _: ContentFile(
+            ImageField()._make_data(
+                {'width': 1024, 'height': 768}
+            ), 'example.jpg'
+        )
+    )
